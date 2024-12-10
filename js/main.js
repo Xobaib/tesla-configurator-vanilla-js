3c2ea0f61e73ade106273441731f2cddba4f47b5
xobaib <xobaib2001.m@gmail.com>
2024-12-10 15:14:05 +0330

    Color Buttons Select Border

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 const topBar = document.getElementById("top-bar");
+const exteriorColorSection = document.getElementById("exterior-buttons");
+const interiorColorSection = document.getElementById("interior-buttons");
+const exteriorImage = document.getElementById("exterior-image");
+const interiorImage = document.getElementById("interior-image");
+
+// console.log(exteriorColorSection);
 
 
-// Handle top bar on scroll
+// Handle Top Bar On Scroll
 function handleScroll() {
     const isAtTop = window.scrollY === 0;
-    // console.log(isAtTop);
     
     // My Way:
     // 
@@ -22,6 +27,35 @@ function handleScroll() {
     // topBar.classList.toggle("goUpAnimate", !isAtTop);
 }
 
+// Handle Color Selection
+function handleColorButtonClick(event) {
+    let button;
+    
+    if(event.target.tagName === "IMG") {
+        button = event.target.closest("button");
+    }else if(event.target.tagName === "BUTTON") {
+        button = event.target;
+    }
+
+    if(button) {
+        const buttons = event.currentTarget.querySelectorAll("button");
+        // My Way:
+        buttons.forEach(btn => {
+            if(btn === button) {
+                btn.classList.add("btn-selected");
+            }else {
+                btn.classList.remove("btn-selected");
+            }
+        });
+
+        // Bard Way
+        // buttons.forEach(btn => btn.classList.remove("btn-selected"));
+        // button.classList.add("btn-selected");
+    }
+}
+
 
 window.addEventListener("scroll", () => requestAnimationFrame(handleScroll));
 
+exteriorColorSection.addEventListener('click', handleColorButtonClick);
+interiorColorSection.addEventListener('click', handleColorButtonClick);
